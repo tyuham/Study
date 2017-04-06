0231b305776e7d08233767b0d8a8d5f89d0ed22c
changhwan <tyu1204@naver.com>
2017-04-06 11:06:35 +0900

    update pushprint.java in local

--- helloworld/src/helloworld/PushPrint.java
+++ helloworld/src/helloworld/PushPrint.java
@@ -4,7 +4,7 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Add Java file");
-		System.out.println("hello in local ");	
+		System.out.println("hello in local");	
 	}
 
 }
